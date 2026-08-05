20d9fe68b31770417e0344df8f4b3f523309df86
haruki7049 <tontonkirikiri@gmail.com>
2026-08-05 11:08:10 +0900

    Update

---
+++ cmd/trump-center/trump-center.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	// internal
+	"github.com/haruki7049/trump-center/internal/game"
+)
+
+func main() {
+	game.Run()
+}
---
+++ internal/game/game.go
@@ -0,0 +1,9 @@
+package game
+
+import "fmt"
+
+func Run() error {
+	fmt.Println("Hello from game.Run")
+
+	return nil
+}
