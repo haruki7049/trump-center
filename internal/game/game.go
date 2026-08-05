03883df79f3fcea7cb22deeaa609e9d4bc2c0d0c
haruki7049 <tontonkirikiri@gmail.com>
2026-08-05 11:22:34 +0900

    Update

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,9 +1,43 @@
 package game
 
-import "fmt"
+import (
+	// externals
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
 
+const WINDOW_WIDTH = 1280
+const WINDOW_HEIGHT = 720
+const TITLE = "Trump Center"
+
+// Run our trump game
 func Run() error {
-	fmt.Println("Hello from game.Run")
+	ebiten.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+	ebiten.SetWindowTitle(TITLE)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	game := NewGame()
+	if err := ebiten.RunGame(game); err != nil {
+		return err
+	}
 
 	return nil
+}
+
+type Game struct{}
+
+func NewGame() *Game {
+	return &Game{}
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, world!!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
 }
