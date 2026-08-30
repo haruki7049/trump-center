9806f4ee0079a5f49d390438d54b5c5ae8776898
haruki7049 <tontonkirikiri@gmail.com>
2026-08-31 07:36:50 +0900

    Update

--- internal/game/scenes/title/title.go
+++ internal/game/scenes/title/title.go
@@ -1,6 +1,10 @@
 package title
 
 import (
+	"image/color"
+
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/widget"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	"github.com/haruki7049/trump-center/assets"
@@ -8,13 +12,10 @@ import (
 	"github.com/haruki7049/trump-center/internal/ui"
 )
 
-const titleMessage = "Trump Center"
-const titleMessageTranslationX = 0
-const titleMessageTranslationY = 0
-
 type TitleScene struct {
 	menuButton *ui.Button
-	fontFace   *text.GoTextFace
+	fontFace   text.Face
+	ui         *ebitenui.UI
 }
 
 // Creates the new TitleScene value with initial member variables
@@ -24,7 +25,18 @@ func NewTitleScene() (*TitleScene, error) {
 		return nil, err
 	}
 
+	ts.newTitleSceneUi()
+
 	return &ts, nil
+}
+
+func (ts *TitleScene) newTitleSceneUi() {
+	rootContainer := widget.NewContainer()
+	eui := &ebitenui.UI{Container: rootContainer}
+
+	helloWorldlabel := widget.NewText(widget.TextOpts.Text("Hello, world!", &ts.fontFace, color.White))
+	rootContainer.AddChild(helloWorldlabel)
+	ts.ui = eui
 }
 
 func (ts *TitleScene) newTitleSceneFontFace() error {
@@ -55,14 +67,12 @@ func loadFont(filename string) (*text.GoTextFace, error) {
 }
 
 func (s *TitleScene) Update() (scene.Scene, error) {
+	s.ui.Update()
 	return nil, nil
 }
 
 func (s *TitleScene) Draw(screen *ebiten.Image) {
-	titleMessageOp := &text.DrawOptions{}
-	titleMessageOp.GeoM.Translate(titleMessageTranslationX, titleMessageTranslationY)
-	titleMessageOp.LineSpacing = s.lineSpacing()
-	text.Draw(screen, titleMessage, s.fontFace, titleMessageOp)
+	s.ui.Draw(screen)
 }
 
 // lineSpacing returns the standard line spacing for fontFace, used by
